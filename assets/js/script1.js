0c74f172189851fcbec90afc438da13e243fcb19
Kansokmean9 <kansokmean006@gmail.com>
2024-12-11 22:28:11 +0700

    mean

---
+++ assets/js/script1.js
@@ -0,0 +1,57 @@
+var id_cus = document.getElementById('id-cus');
+var name_cus = document.getElementById('name-cus');
+var old_num = document.getElementById('old-num');
+var new_num = document.getElementById('new-num');
+var trash = document.getElementById('trash');
+var costTrash = document.getElementById('costTrash');
+
+function edcCal() {
+    document.getElementById('boxempty').classList.add('d-none');
+    document.getElementById('invoice').classList.remove('d-none');
+    var getID = document.getElementById('getID');
+    var getName = document.getElementById('getName');
+    var getOldnum = document.getElementById('getOldnum');
+    var getNewnum = document.getElementById('getNewnum');
+    var getTrashCost = document.getElementById('getTrashCost');
+    var amountCost = 0;
+    var costPer = 0;
+    
+    var usePerMonth = parseFloat(new_num.value) - parseFloat(old_num.value); 
+    if(usePerMonth > 0 && usePerMonth < 51){
+        costPer = 500;
+    }else if(usePerMonth <= 100){
+        costPer = 1000;
+    }else if(usePerMonth <= 150){
+        costPer = 1500;
+    }else if(usePerMonth <= 200){
+        costPer = 2000;
+    }else if(usePerMonth > 200){
+        costPer = 2500;
+    }else{
+        costPer = 1;
+    }
+    amountCost = parseFloat(usePerMonth) * parseFloat(costPer);    
+
+    getID.innerHTML = id_cus.value || 0;
+    getName.innerHTML = name_cus.value || 0;
+    getOldnum.innerHTML = old_num.value || 0;
+    getNewnum.innerHTML = new_num.value || 0;
+    var TrashCost = 0;
+    if (trash.checked) {
+        getTrashCost.innerHTML =  costTrash.value + "៛";
+        TrashCost = costTrash.value;
+    } else {
+        TrashCost = 0;
+    }
+    document.getElementById('total').innerHTML = (amountCost + parseFloat(TrashCost)) +"៛";
+
+}
+
+function btnclear() {
+    document.getElementById('boxempty').classList.remove('d-none');
+    document.getElementById('invoice').classList.add('d-none');
+    id_cus.value = "";
+    name_cus.value = "";
+    old_num.value = "";
+    new_num.value = "";
+}
---
+++ assets/js/script2.js
@@ -0,0 +1,46 @@
+function edc2() {
+    alert('សូមស្វាគមន៍មកកាន់ Website អគ្គីសនីជាតិកម្ពុជា');
+    var id_cus = prompt('បញ្ចូលលេខសម្គាល់អតិថិជន:');
+    var name_cus = prompt('បញ្ចូលឈ្មោះអតិថិជន:');
+    var old_num = prompt('បញ្ចូលអំណានចាស់:');
+    var new_num = prompt('បញ្ចូលអំណានថ្មី:');
+    var trash = confirm("តើអ្នកចង់បង់ថ្លៃសំរាមដែរឬទេ?");
+    var costTrash = 0;
+    if (trash) {
+        costTrash = prompt('សូមបញ្ចូលតម្លៃសម្រាម(10000៛/20000៛/30000៛):');
+    } else {
+        costTrash = 0;
+    }
+    var getID = document.getElementById('getID');
+    var getName = document.getElementById('getName');
+    var getOldnum = document.getElementById('getOldnum');
+    var getNewnum = document.getElementById('getNewnum');
+    var getusePermonth = document.getElementById('getusePermonth');
+    var amountCost = 0;
+    var costPer = 0;
+
+    var usePerMonth = parseFloat(new_num) - parseFloat(old_num);
+    if (usePerMonth > 0 && usePerMonth < 51) {
+        costPer = 500;
+    } else if (usePerMonth <= 100) {
+        costPer = 1000;
+    } else if (usePerMonth <= 150) {
+        costPer = 1500;
+    } else if (usePerMonth <= 200) {
+        costPer = 2000;
+    } else if (usePerMonth > 200) {
+        costPer = 2500;
+    } else {
+        costPer = 1;
+    }
+    amountCost = parseFloat(usePerMonth) * parseFloat(costPer);
+
+    getID.innerHTML = id_cus || 0;
+    getName.innerHTML = name_cus || 0;
+    getOldnum.innerHTML = old_num || 0;
+    getNewnum.innerHTML = new_num || 0;
+    getusePermonth.innerHTML = usePerMonth || 0;
+    getTrashCost.innerHTML = costTrash + "៛";
+    document.getElementById('total').innerHTML = (amountCost + parseFloat(costTrash)) + "៛";
+}
+edc2();
